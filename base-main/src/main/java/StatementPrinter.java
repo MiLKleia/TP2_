b4c9d78ab5ea3f340b8a5ccc424194bab88b243f
MiLKleia <ines.meyre@gmail.com>
2023-10-20 13:43:56 +0200

    create 2 functions in StPr.java and modified print

--- base-main/src/main/java/StatementPrinter.java
+++ base-main/src/main/java/StatementPrinter.java
@@ -3,6 +3,42 @@ import java.util.*;
 
 public class StatementPrinter {
 
+  private int getAmount(Performance perf, String type) {
+    int amount_out = 0; 
+    switch (type) {
+      default : 
+        throw new Error("unknown type: ${play.type}");
+
+      case "tragedy":
+        amount_out = 40000 + (1000 * Math.max(0,perf.audience - 30));
+        break;
+
+      case "comedy":
+        amount_out = 30000 + 300 * perf.audience;
+        if (perf.audience > 20) {
+          amount_out += 10000 + 500 * (perf.audience - 20);
+        }
+        break;
+    }
+    return amount_out;
+  }
+
+  private int getVolumeCredit(Performance perf, String type) {
+    int volumeCredit_out = 0; 
+    
+    volumeCredit_out += Math.max(perf.audience - 30, 0);
+    switch (type){
+      case "comedy":
+        volumeCredit_out += Math.floor(perf.audience / 5);
+    }
+    return volumeCredit_out;
+  }
+
+
+
+
+
+
   public String print(Invoice invoice, HashMap<String, Play> plays) {
     int totalAmount = 0;
     int volumeCredits = 0;
@@ -12,30 +48,10 @@ public class StatementPrinter {
 
     for (Performance perf : invoice.performances) {
       Play play = plays.get(perf.playID);
-      int thisAmount = 0;
 
-      switch (play.type) {
-        case "tragedy":
-          thisAmount = 40000;
-          if (perf.audience > 30) {
-            thisAmount += 1000 * (perf.audience - 30);
-          }
-          break;
-        case "comedy":
-          thisAmount = 30000;
-          if (perf.audience > 20) {
-            thisAmount += 10000 + 500 * (perf.audience - 20);
-          }
-          thisAmount += 300 * perf.audience;
-          break;
-        default:
-          throw new Error("unknown type: ${play.type}");
-      }
+      int thisAmount = getAmount( perf,  play.type);
 
-      // add volume credits
-      volumeCredits += Math.max(perf.audience - 30, 0);
-      // add extra credit for every ten comedy attendees
-      if ("comedy".equals(play.type)) volumeCredits += Math.floor(perf.audience / 5);
+      volumeCredits += getVolumeCredit( perf,  play.type);
 
       // print line for this order
       result += String.format("  %s: %s (%s seats)\n", play.name, frmt.format(thisAmount / 100), perf.audience);
---
+++ base-main/src/main/java/ticket.java
@@ -0,0 +1,14 @@
+public class ticket {
+
+    public int Amount;
+    public int Volume;
+    private String recipe;
+
+    public ticket(){
+        this.Amount = 0;
+        this.Volume = 0;
+        this.recipe ="";
+
+    }
+    
+}
